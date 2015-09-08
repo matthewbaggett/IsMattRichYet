66e25984a1aeda80bcbe2d3ad23e4adc77bbfd90
Matthew Baggett <matthew@baggett.me>
2015-09-08 18:46:54 +0100

    Fix styling bugs

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-$summary = file_get_contents("http://bankapi.thru.io/summary/Matthew Baggett");
+$summary = file_get_contents("http://bankapi.thru.io/summary/Matthew%20Baggett");
 $summary = json_decode($summary);
-var_dump($summary);
 $answer = ($summary->total>1000)?'Yes':'No';
 $tagline = "£{$summary->total}";
 // Render the template.
