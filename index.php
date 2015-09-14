69f8ccee427daca20b36f1a7e3696069f4758cf3
Matthew Baggett <matthew@baggett.me>
2015-09-14 03:19:25 +0100

    Fix "Â£" issue.

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $summary = file_get_contents("http://bankapi.thru.io/summary/Matthew%20Baggett");
 $summary = json_decode($summary);
 $answer = ($summary->total>1000)?'Yes':'No';
-$tagline = "£{$summary->total}";
+$tagline = "&pound; {$summary->total}";
 // Render the template.
 require("template.phtml");
